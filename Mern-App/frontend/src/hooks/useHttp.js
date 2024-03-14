ae1d2320a92be5e6826c722e9362da547b03c160
badrudeen20 <badruddinansary20@gmail.com>
2024-03-14 15:44:35 +0530

    url-change-2

--- Mern-App/frontend/src/hooks/useHttp.js
+++ Mern-App/frontend/src/hooks/useHttp.js
@@ -1,7 +1,7 @@
 export const useHttp = () => {
   /* https://dtrm9n-5000.csb.app */
   const http = async (url,parms) => {
-    const response = await fetch('https://gt782v.csb.app'+url, parms)
+    const response = await fetch('http://localhost:5000'+url, parms)
     const json =  await response.json()
     return {
       response,json
