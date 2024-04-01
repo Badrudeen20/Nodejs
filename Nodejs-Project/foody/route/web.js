419ca6829ca61068825e274d6b4e68e1ce1317cc
badrudeen20 <badruddinansary20@gmail.com>
2024-04-01 20:00:51 +0530

    foody6

--- Nodejs-Project/foody/route/web.js
+++ Nodejs-Project/foody/route/web.js
@@ -1,16 +1,16 @@
 const express = require('express');
-/* const DashboardController = require('../app/controller/backend/DashboardController'); */
+const DashboardController = require('../app/controller/backend/DashboardController');
 const AuthController = require('../app/controller/AuthController');
 const { adminForwardAuthenticated,userForwardAuthenticated,adminEnsureAuthenticated,userEnsureAuthenticated } = require('../app/config/auth');
-// const  group = require('../app/helper/group');
+const  group = require('../app/helper/group');
 const HomeController = require('../app/controller/frontend/HomeController');
 
-/* const CustomerController = require('../app/controller/backend/CustomerController');
+const CustomerController = require('../app/controller/backend/CustomerController');
 const OrderController = require('../app/controller/backend/OrderController');
 const ProductController = require('../app/controller/backend/ProductController');
 const ShopController = require('../app/controller/frontend/ShopController');
 const CartController = require('../app/controller/frontend/CartController');
-const BrandController = require('../app/controller/backend/BrandController'); */
+const BrandController = require('../app/controller/backend/BrandController');
 
 
 const router = express.Router()
@@ -22,7 +22,7 @@ router.all('/login',userForwardAuthenticated,AuthController.login);
 router.all('/register',userForwardAuthenticated,AuthController.register);
 // Backend Controller
 
-/* router.use("/admin",adminEnsureAuthenticated, group((router) => {
+router.use("/admin",adminEnsureAuthenticated, group((router) => {
       router.get('/dashboard',DashboardController.view); 
       router.get('/customers',CustomerController.view); 
       router.get('/customers/list',CustomerController.list); 
@@ -49,28 +49,28 @@ router.all('/register',userForwardAuthenticated,AuthController.register);
                   }
             });  
       });
-})); */
+}));
 
 
 // Frontend Controller
 router.all('/',HomeController.home);
-/* router.get('/home',HomeController.home);
+router.get('/home',HomeController.home);
 router.get('/shop',ShopController.shop);
 router.get('/shop-detail/:id',ShopController.detail);
-router.get('/contact',HomeController.contact); */
+router.get('/contact',HomeController.contact);
 
 //Auth Frontend Controller
 
 
-/* router.use("/user", userEnsureAuthenticated, group((router) => {
-      router.get('/cart',userEnsureAuthenticated,CartController.cart);
-      router.post('/quantity/:id',userEnsureAuthenticated,CartController.quantity);
-      router.post('/coupon',userEnsureAuthenticated,CartController.coupon);
-      router.all('/addCart/:id',userEnsureAuthenticated,CartController.addCart);
-      router.get('/checkout',userEnsureAuthenticated,CartController.checkout);
-      router.get('/delete/:id',userEnsureAuthenticated,CartController.delete);
-      router.post('/success',userEnsureAuthenticated,CartController.success);
-      router.get('/logout',userEnsureAuthenticated,function(req,res){
+router.use(userEnsureAuthenticated, group((router) => {
+      router.get('/cart',CartController.cart);
+      router.post('/quantity/:id',CartController.quantity);
+      router.post('/coupon',CartController.coupon);
+      router.all('/addCart/:id',CartController.addCart);
+      router.get('/checkout',CartController.checkout);
+      router.get('/delete/:id',CartController.delete);
+      router.post('/success',CartController.success);
+      router.get('/logout',function(req,res){
             req.logout(function(err){
                   if (err) {
                         req.flash('error_msg', 'Failed to logout');
@@ -81,7 +81,7 @@ router.get('/contact',HomeController.contact); */
             });
             
       });
-})); */
+}));
 
 
 
