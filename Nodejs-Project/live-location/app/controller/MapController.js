4eeeaece53227875c42c9cf6f3ea0c9d12d3ce6c
badrudeen20 <badruddinansary20@gmail.com>
2024-04-20 20:59:45 +0530

    live-location-2

--- Nodejs-Project/live-location/app/controller/MapController.js
+++ Nodejs-Project/live-location/app/controller/MapController.js
@@ -1,11 +1,3 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient()
-const http = require("http");
-const express = require("express");
-const { Server } = require("socket.io");
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
 module.exports = {
   view: async function (req, res) {
     res.render("index");
