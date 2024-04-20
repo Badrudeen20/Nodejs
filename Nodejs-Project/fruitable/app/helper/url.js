44ef6229c56726dc3587c5914302b8f3ee3abf37
badrudeen20 <badruddinansary20@gmail.com>
2024-04-20 14:39:06 +0530

    fruitable-30

--- Nodejs-Project/fruitable/app/helper/url.js
+++ Nodejs-Project/fruitable/app/helper/url.js
@@ -4,7 +4,7 @@ const path = require('path')
 const rootDir = path.resolve(__dirname, '../../');
 module.exports = {
       url:function(req,res){
-           return `http://${req.get('host')}/`; 
+           return `https://${req.get('host')}/`; 
       },
       rootPath:(function(path){
           return path
