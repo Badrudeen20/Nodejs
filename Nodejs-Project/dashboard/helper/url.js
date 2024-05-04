8c9ad909101d10abcde8253588cfda63d82bf3d9
badrudeen20 <badruddinansary20@gmail.com>
2024-05-04 16:26:26 +0530

    form-builder-16

--- Nodejs-Project/dashboard/helper/url.js
+++ Nodejs-Project/dashboard/helper/url.js
@@ -2,7 +2,7 @@ const path = require('path')
 const rootDir = path.resolve(__dirname, '../../');
 module.exports = {
       url:function(req,res){
-           return `http://${req.get('host')}/`; 
+           return `https://${req.get('host')}/`; 
       },
       rootPath:(function(path){
           return path
