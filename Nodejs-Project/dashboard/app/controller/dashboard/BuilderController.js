1ac542b2006c19b6c6c59842f47f0dcab25f948c
badrudeen20 <badruddinansary20@gmail.com>
2024-05-04 16:46:42 +0530

    form-builder-17

--- Nodejs-Project/dashboard/app/controller/dashboard/BuilderController.js
+++ Nodejs-Project/dashboard/app/controller/dashboard/BuilderController.js
@@ -4,23 +4,14 @@ const prisma = new PrismaClient();
 module.exports = {
       
   builder:async function(req,res){
-    // const forms = await prisma.dynamicform.findMany({
-    //   where:{status:'ACTIVE'}
-    // })
-    // return res.render('Backend/builder',{ 
-    //   layout: 'Backend/layout',
-    //   forms
-    // })
-    try {
-      const forms = await prisma.dynamicform.findMany({});
-      console.log(forms)
-      return res.render('Backend/builder',{ 
+    const forms = await prisma.dynamicform.findMany({
+      where:{status:'ACTIVE'}
+    })
+    return res.render('Backend/builder',{ 
       layout: 'Backend/layout',
       forms
     })
-    } catch (error) {
-      console.error('Error:', error);
-    }
+    
   },
   
   addForm:async function(req,res){
