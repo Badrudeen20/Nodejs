8e80e19c640e7d53cda53c3cd136ca0b947051b9
badrudeen20 <badruddinansary20@gmail.com>
2024-05-04 19:33:23 +0530

    form-builder-20

--- Nodejs-Project/dashboard/prisma/migrations/20240504135707_add_tables/migration.sql
+++ Nodejs-Project/dashboard/prisma/migrations/20240504135707_add_tables/migration.sql
@@ -1,4 +1,14 @@
 -- CreateTable
+CREATE TABLE "dynamicforms" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "form" TEXT,
+    "status" TEXT NOT NULL DEFAULT 'ACTIVE',
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL
+);
+
+-- CreateTable
 CREATE TABLE "products" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "name" TEXT NOT NULL,
@@ -10,6 +20,9 @@ CREATE TABLE "products" (
     "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" DATETIME NOT NULL
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "dynamicforms_name_key" ON "dynamicforms"("name");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "products_name_key" ON "products"("name");
